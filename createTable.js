95f459161b9e3800ac47d3886f696f4e78ba300b
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-13 22:30:47 -0400

    k

--- createTable.js
+++ createTable.js
@@ -3,7 +3,7 @@ function criarTabela()
 {
     var tabela = document.getElementsByClassName("tabela")[0];
     var dados;       
-    for(let i = 0; i<basedeDados[i].length; i++)
+    for(let i = 0; i<basedeDados.length; i++)
     {
         dados += "<tr><td>" + basedeDados[i][0] + "<td><tr>";
     }

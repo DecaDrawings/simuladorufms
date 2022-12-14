5cd0ece8be04551a3a5ab30452e772741ec48e4f
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-13 22:35:33 -0400

    K

--- createTable.js
+++ createTable.js
@@ -5,13 +5,13 @@ function criarTabela()
     var dados;       
     for(let i = 0; i<basedeDados.length; i++)
     {
-        dados += "<tr><td>" + basedeDados[i][0] + "<td><tr>"
-                 "<tr><td>" + basedeDados[i][1] + "<td><tr>"
-                 "<tr><td>" + basedeDados[i][2] + "<td><tr>"
-                 "<tr><td>" + basedeDados[i][3] + "<td><tr>"
-                 "<tr><td>" + basedeDados[i][4] + "<td><tr>"
-                 "<tr><td>" + basedeDados[i][5] + "<td><tr>"
-                 "<tr><td>" + basedeDados[i][6] + "<td><tr>";
+        dados += "<tr><td>" + basedeDados[i][0] + "<td>" +
+                 "<td>" + basedeDados[i][1] + "<td>" + 
+                 "<td>" + basedeDados[i][2] + "<td>" +
+                 "<td>" + basedeDados[i][3] + "<td>" +
+                 "<td>" + basedeDados[i][4] + "<td>" +
+                 "<td>" + basedeDados[i][5] + "<td>" +
+                 "<td>" + basedeDados[i][6] + "<td></tr>";        
     }
     tabela.innerHTML = dados;    
 }

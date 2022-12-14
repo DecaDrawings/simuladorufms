8342908daf79fe4da8eeb691209d3f79d911a325
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-13 23:09:23 -0400

    k

--- createTable.js
+++ createTable.js
@@ -6,23 +6,8 @@ input.addEventListener("keyup", function(){
 });
 
 function criarTabela(numInsc)
-{
-    if(!numInsc)
-    {
-        var tabela = document.getElementsByClassName("tabela")[0];
-        var dados = "<tr><th>Inscrição</th>" + "<th>Modalidade</th>" + "<th>PR</th><th>PM</th><th>PL</th><th>PH</th><th>PN</th></tr>";       
-        for(let i = 0; i<basedeDados.length; i++)
-        {
-            dados += "<tr><td>" + basedeDados[i][0] + "</td>" +
-                     "<td>" + basedeDados[i][1] + "</td>" + 
-                     "<td>" + basedeDados[i][2] + "</td>" +
-                     "<td>" + basedeDados[i][3] + "</td>" +
-                     "<td>" + basedeDados[i][4] + "</td>" +
-                     "<td>" + basedeDados[i][5] + "</td>" +
-                     "<td>" + basedeDados[i][6] + "</td></tr>";        
-        }
-    }
-    else if (numInsc != "")
+{    
+    if (numInsc != "")
     {
         var tabela = document.getElementsByClassName("tabela")[0];
         var dados = "<tr><th>Inscrição</th>" + "<th>Modalidade</th>" + "<th>PR</th><th>PM</th><th>PL</th><th>PH</th><th>PN</th></tr>";       
@@ -39,6 +24,21 @@ function criarTabela(numInsc)
                      "<td>" + basedeDados[i][6] + "</td></tr>"; 
             }
             i = basedeDados.length;                  
+        }
+    }
+    else
+    {
+        var tabela = document.getElementsByClassName("tabela")[0];
+        var dados = "<tr><th>Inscrição</th>" + "<th>Modalidade</th>" + "<th>PR</th><th>PM</th><th>PL</th><th>PH</th><th>PN</th></tr>";       
+        for(let i = 0; i<basedeDados.length; i++)
+        {
+            dados += "<tr><td>" + basedeDados[i][0] + "</td>" +
+                     "<td>" + basedeDados[i][1] + "</td>" + 
+                     "<td>" + basedeDados[i][2] + "</td>" +
+                     "<td>" + basedeDados[i][3] + "</td>" +
+                     "<td>" + basedeDados[i][4] + "</td>" +
+                     "<td>" + basedeDados[i][5] + "</td>" +
+                     "<td>" + basedeDados[i][6] + "</td></tr>";        
         }
     }
     tabela.innerHTML = dados;    

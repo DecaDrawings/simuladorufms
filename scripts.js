6a864a861445c3a3e39d3c7798ed1db781057179
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-16 21:52:42 -0400

    K

--- scripts.js
+++ scripts.js
@@ -20,14 +20,16 @@ var EPLIN;
 var mediaFinal;
 
 var resultadoBox = document.getElementsByClassName("formulario_box")[0];
-
+window.addEventListener('DOMContentLoaded', (event) => {
+    carregarFoot();
+});
 function carregarFoot()
 {	
-var navTamanho = document.getElementsByClassName("nav")[0].clientHeight;
-var contentTamanho = document.getElementsByClassName("content")[0].clientHeight;
-var foot = document.getElementsByClassName("foot")[0];
-var viewport_height = window.innerHeight;
-foot.style.height = viewport_height - (contentTamanho + navTamanho) + "px";
+	var navTamanho = document.getElementsByClassName("nav")[0].clientHeight;
+	var contentTamanho = document.getElementsByClassName("content")[0].clientHeight;
+	var foot = document.getElementsByClassName("foot")[0];
+	var viewport_height = window.innerHeight;
+	foot.style.height = viewport_height - (contentTamanho + navTamanho) + "px";
 }
 
 

198da1fd6055916fcfba379920af2e5f8911057c
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-17 21:11:47 -0400

    K

--- translateJS.js
+++ translateJS.js
@@ -3,23 +3,17 @@ function tranlateScrollBox()
 {
     var scrollBox = document.getElementsByClassName("scroll-box")[0];
     var scrollBox1 = document.getElementsByClassName("scroll-box")[1];
-    var scrollBox2 = document.getElementsByClassName("scroll-box")[2];
-    var scrollBoxW = scrollBox.getBoundingClientRect().width;
-    var scrollBox1W = scrollBox1.getBoundingClientRect().width;
-    var scrollBox2W = scrollBox2.getBoundingClientRect().width;    
-    const parentWidth = scrollBox.parentElement.getBoundingClientRect().width;
-    const parentWidth1 = scrollBox1.parentElement.getBoundingClientRect().width;
-    const parentWidth2 = scrollBox2.parentElement.getBoundingClientRect().width;
+    var scrollBox2 = document.getElementsByClassName("scroll-box")[2];   
     
-    if (scrollBox.scrollWidth > scrollBox.clientWidth || scrollBox.scrollWidth > scrollBox.scrollWidth)
+    if (scrollBox.scrollWidth > scrollBox.clientWidth)
     {
         console.log("1 é overflow")
     }
-    if (scrollBox1.scrollWidth > scrollBox1.clientWidth || scrollBox1.scrollWidth > scrollBox1.scrollWidth)
+    if (scrollBox1.scrollWidth > scrollBox1.clientWidth)
     {
         console.log("0 é overflow")
     }
-    if (scrollBox2.scrollWidth > scrollBox2.clientWidth || scrollBox2.scrollWidth > scrollBox2.scrollWidth)
+    if (scrollBox2.scrollWidth > scrollBox2.clientWidth)
     {
         console.log("2 é overflow")
     }

4d783ad59cacd1a7ff29d8c221661a06a3bd26cc
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-18 00:44:26 -0400

    g

--- translateJS.js
+++ translateJS.js
@@ -16,9 +16,19 @@ var elementoPai = scrollBox.parentElement;
 var elementoPai1 = scrollBox1.parentElement;
 var elementoPai2 = scrollBox2.parentElement;
 
-
-
-
+for(var i = 0; i < quantasVezes; i++)
+{
+    elementoPai.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
+    
+}
+for(var i = 0; i < quantasVezes1; i++)
+{
+    elementoPai1.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
+}
+for(var i = 0; i < quantasVezes2; i++)
+{
+    elementoPai2.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
+}
 
 
 function tranlateScrollBox()
@@ -29,10 +39,6 @@ function tranlateScrollBox()
         {
             scrollBox.style.transform = "translate(-" + (contador * 100) + "%)";
             contador += 1;
-            for(var i = 0; i < quantasVezes; i++)
-            {
-                elementoPai.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
-            }
             if(contador == quantasVezes)
             {
                 contador = 0;
@@ -45,10 +51,6 @@ function tranlateScrollBox()
         {
             scrollBox1.style.transform = "translate(-" + (contador1 * 100) + "%)";
             contador1 += 1;
-            for(var i = 0; i < quantasVezes1; i++)
-            {
-                elementoPai1.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
-            }
             if(contador1 == quantasVezes1)
             {
                 contador1 = 0;
@@ -61,10 +63,6 @@ function tranlateScrollBox()
         {
             scrollBox2.style.transform = "translate(-" + (contador2 * 100) + "%)";
             contador2 += 1;
-            for(var i = 0; i < quantasVezes2; i++)
-            {
-                elementoPai2.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
-            }
             if(contador2 == quantasVezes2)
             {
                 contador2 = 0;

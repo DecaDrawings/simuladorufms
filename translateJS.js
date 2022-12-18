59f189d09592ff7a18c4d5f8976aa45408b36717
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-18 00:42:22 -0400

    h

--- translateJS.js
+++ translateJS.js
@@ -11,6 +11,16 @@ var quantasVezes2 = Math.round(scrollBox2.scrollWidth / scrollBox2.clientWidth);
 var contador = 1;
 var contador1 = 1;
 var contador2 = 1;
+
+var elementoPai = scrollBox.parentElement;
+var elementoPai1 = scrollBox1.parentElement;
+var elementoPai2 = scrollBox2.parentElement;
+
+
+
+
+
+
 function tranlateScrollBox()
 {     
     if (scrollBox.scrollWidth > scrollBox.clientWidth)
@@ -19,6 +29,10 @@ function tranlateScrollBox()
         {
             scrollBox.style.transform = "translate(-" + (contador * 100) + "%)";
             contador += 1;
+            for(var i = 0; i < quantasVezes; i++)
+            {
+                elementoPai.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
+            }
             if(contador == quantasVezes)
             {
                 contador = 0;
@@ -31,6 +45,10 @@ function tranlateScrollBox()
         {
             scrollBox1.style.transform = "translate(-" + (contador1 * 100) + "%)";
             contador1 += 1;
+            for(var i = 0; i < quantasVezes1; i++)
+            {
+                elementoPai1.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
+            }
             if(contador1 == quantasVezes1)
             {
                 contador1 = 0;
@@ -43,6 +61,10 @@ function tranlateScrollBox()
         {
             scrollBox2.style.transform = "translate(-" + (contador2 * 100) + "%)";
             contador2 += 1;
+            for(var i = 0; i < quantasVezes2; i++)
+            {
+                elementoPai2.innerHTML = "<span class='.span' style='"+(10*i)+"px;'></span>";
+            }
             if(contador2 == quantasVezes2)
             {
                 contador2 = 0;

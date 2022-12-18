e688407f873409bfb0bd751de6e8b821ccc588c7
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-17 21:00:41 -0400

    K

--- translateJS.js
+++ translateJS.js
@@ -1,18 +1,21 @@
 tranlateScrollBox();
 function tranlateScrollBox()
 {
-    var scrollBox = document.getElementsByClassName("scroll-box")[0];
-    var scrollBox1 = document.getElementsByClassName("scroll-box")[1];
-    var scrollBox2 = document.getElementsByClassName("scroll-box")[2];
-    if (scrollBox1.scrollHeight != Math.max(scrollBox1.offsetHeight, scrollBox1.clientHeight))
+    var scrollBox = document.getElementsByClassName("scroll-box")[0].width;
+    var scrollBox1 = document.getElementsByClassName("scroll-box")[1].width;
+    var scrollBox2 = document.getElementsByClassName("scroll-box")[2].width;    
+    const parentWidth = scrollBox.parentElement.getBoundingClientRect().width;
+    const parentWidth1 = scrollBox1.parentElement.getBoundingClientRect().width;
+    const parentWidth2 = scrollBox2.parentElement.getBoundingClientRect().width;
+    if (scrollBox > parentWidth)
     {
         console.log("1 é overflow")
     }
-    if (scrollBox.scrollHeight != Math.max(scrollBox.offsetHeight, scrollBox.clientHeight))
+    if (scrollBox1 > parentWidth1)
     {
         console.log("0 é overflow")
     }
-    if (scrollBox2.scrollHeight != Math.max(scrollBox2.offsetHeight, scrollBox2.clientHeight))
+    if (scrollBox2 > parentWidth2)
     {
         console.log("2 é overflow")
     }

88adf70506cddb9776d065d77a6cf403ad57e01b
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-17 21:23:45 -0400

    K

--- translateJS.js
+++ translateJS.js
@@ -11,10 +11,10 @@ function tranlateScrollBox()
         var translate = scrollBox.style.translate;
         if(scrollBox.translate = "-100%")
         {
-            scrollBox.style.transform = "translate(100%);"
+            scrollBox.style.transform = "translate(100%)"
         }
         else{
-            scrollBox.style.transform = "translate(-100%);"
+            scrollBox.style.transform = "translate(-100%)"
         }        
     }
     if (scrollBox1.scrollWidth > scrollBox1.clientWidth)
@@ -23,10 +23,10 @@ function tranlateScrollBox()
         var translate = scrollBox1.style.translate;
         if(scrollBox1.translate = "-100%")
         {
-            scrollBox1.style.transform = "translate(100%);"
+            scrollBox1.style.transform = "translate(100%)"
         }
         else{
-            scrollBox1.style.transform = "translate(-100%);"
+            scrollBox1.style.transform = "translate(-100%)"
         }        
     }
     if (scrollBox2.scrollWidth > scrollBox2.clientWidth)
@@ -35,10 +35,10 @@ function tranlateScrollBox()
         var translate = scrollBox2.style.translate;
         if(scrollBox2.translate = "-100%")
         {
-            scrollBox2.style.transform = "translate(100%);"
+            scrollBox2.style.transform = "translate(100%)"
         }
         else{
-            scrollBox2.style.transform = "translate(-100%);"
+            scrollBox2.style.transform = "translate(-100%)"
         }        
     }
 }

6f15ba8c3e3f21d58afe5b2b1c12627ac0dff5db
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-17 21:19:55 -0400

    K

--- translateJS.js
+++ translateJS.js
@@ -20,25 +20,25 @@ function tranlateScrollBox()
     if (scrollBox1.scrollWidth > scrollBox1.clientWidth)
     {
         console.log("1 é overflow")
-        var translate = scrollBox.style.translate;
-        if(scrollBox.translate = "-100%")
+        var translate = scrollBox1.style.translate;
+        if(scrollBox1.translate = "-100%")
         {
-            scrollBox.style.transform = "translate(100%);"
+            scrollBox1.style.transform = "translate(100%);"
         }
         else{
-            scrollBox.style.transform = "translate(-100%);"
+            scrollBox1.style.transform = "translate(-100%);"
         }        
     }
     if (scrollBox2.scrollWidth > scrollBox2.clientWidth)
     {
         console.log("1 é overflow")
-        var translate = scrollBox.style.translate;
-        if(scrollBox.translate = "-100%")
+        var translate = scrollBox2.style.translate;
+        if(scrollBox2.translate = "-100%")
         {
-            scrollBox.style.transform = "translate(100%);"
+            scrollBox2.style.transform = "translate(100%);"
         }
         else{
-            scrollBox.style.transform = "translate(-100%);"
+            scrollBox2.style.transform = "translate(-100%);"
         }        
     }
 }

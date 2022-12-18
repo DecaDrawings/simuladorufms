32af0170290f7a2b0438f901cd686bca10e51fab
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-17 21:26:00 -0400

    K

--- translateJS.js
+++ translateJS.js
@@ -9,7 +9,7 @@ function tranlateScrollBox()
     {
         console.log("1 é overflow")
         var translate = scrollBox.style.translate;
-        if(scrollBox.translate = "-100%")
+        if(scrollBox.translate == "-100%")
         {
             scrollBox.style.transform = "translate(100%)"
         }
@@ -21,7 +21,7 @@ function tranlateScrollBox()
     {
         console.log("1 é overflow")
         var translate = scrollBox1.style.translate;
-        if(scrollBox1.translate = "-100%")
+        if(scrollBox1.translate == "-100%")
         {
             scrollBox1.style.transform = "translate(100%)"
         }
@@ -33,7 +33,7 @@ function tranlateScrollBox()
     {
         console.log("1 é overflow")
         var translate = scrollBox2.style.translate;
-        if(scrollBox2.translate = "-100%")
+        if(scrollBox2.translate == "-100%")
         {
             scrollBox2.style.transform = "translate(100%)"
         }

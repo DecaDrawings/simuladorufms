e9f7d4831b3ecbf2848c5d90f4b128ad324648cb
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-17 21:37:55 -0400

    K

--- translateJS.js
+++ translateJS.js
@@ -13,12 +13,13 @@ function tranlateScrollBox()
         var translate = scrollBox.style.translate;
         if(checarTranslado == false)
         {
-            scrollBox.style.transform = "translate(100%)";
+            scrollBox.style.transform = "translate(-100%)";
             checarTranslado = true;
         }
         else
         {
-            scrollBox.style.transform = "translate(-100%)";
+            scrollBox.style.transform = "translate(100%)";
+            checarTranslado = false;
         }        
     }
     if (scrollBox1.scrollWidth > scrollBox1.clientWidth)
@@ -27,26 +28,28 @@ function tranlateScrollBox()
         var translate = scrollBox1.style.translate;
         if(checarTranslado1 == false)
         {
-            scrollBox1.style.transform = "translate(100%)";
+            scrollBox1.style.transform = "translate(-100%)";
             checarTranslado1 = true;
         }
         else
         {
-            scrollBox1.style.transform = "translate(-100%)";
+            scrollBox1.style.transform = "translate(100%)";
+            checarTranslado1 = false;
         }        
     }
     if (scrollBox2.scrollWidth > scrollBox2.clientWidth)
     {
         console.log("1 é overflow")
         var translate = scrollBox2.style.translate;
-        if(checarTranslado1 == false)
+        if(checarTranslado2 == false)
         {
-            scrollBox2.style.transform = "translate(100%)";
-            checarTranslado1 = true;
+            scrollBox2.style.transform = "translate(-100%)";
+            checarTranslado2 = true;
         }
         else
         {
-            scrollBox2.style.transform = "translate(-100%)";
+            scrollBox2.style.transform = "translate(100%)";
+            checarTranslado2 = false;
         }        
     }
 }
